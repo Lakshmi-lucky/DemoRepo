2542670ddda7814409f9d2102c1a2e00747ce52d
Lakshmi-lucky <54809142+Lakshmi-lucky@users.noreply.github.com>
2019-09-22 18:29:20 +0530

    Update Sample.java

--- Git_Workspace/Demo/src/com/varam/trials/Sample.java
+++ Git_Workspace/Demo/src/com/varam/trials/Sample.java
@@ -5,5 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		
 		System.out.println("Trying to understand git...");
+		System.out.println("Editing from github to pull this to local..");
+		
 	}
 }
